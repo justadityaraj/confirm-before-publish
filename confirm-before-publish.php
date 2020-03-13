d3916a8d9c67105f6b3e672e8ac4d254e02883ed
Aditya Raj <45910873+justadityaraj@users.noreply.github.com>
2020-03-13 22:40:29 +0530

    Update confirm-before-publish.php

--- confirm-before-publish.php
+++ confirm-before-publish.php
@@ -1,1 +1,11 @@
+<?php
 
+defined('ABSPATH') || exit;
+
+if (!is_admin()) {
+	return;
+}
+
+require_once dirname(__FILE__) . '/ar/confirm-before-publish.php';
+
+add_action('admin_init', array(Publish_Confirm::get_instance(), 'setup'));
